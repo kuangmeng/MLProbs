1b1e405a4fba6c2739a99358143657c5cc82d665
kuangmeng <kuangmeng@msn.com>
2020-06-29 20:32:49 +0800

    Renew models

--- TEST/script.py
+++ TEST/script.py
@@ -64,6 +64,6 @@ def Compute(prom):
 if __name__ == "__main__":
     if not os.path.exists("./output"):
         os.system("mkdir ./output/")
-    tmp_bench, total_time = Compute("python ../MLProbs.py ")
+    tmp_bench, total_time = Compute("python ./MLProbs.py ")
     for i in range(len(tmp_bench)):
         print("%s: %f" % (tmp_bench[i], total_time[i]))
